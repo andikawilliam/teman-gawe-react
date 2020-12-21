8a5460ab161af2424586c631725ce059df881b4a
vandercoder <andikawilliam@outlook.com>
2020-12-21 23:30:15 +0700

    fix favicon import

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,34 @@
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
+import Head from 'next/head'
+
 import '../styles/index.css'
 import '../styles/tailwind.css'
 import '../styles/coffee-cup.css'
 import '../styles/nav-burger.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Teman Gawe Coffee</title>
+        <link rel="icon" href="/public/favicon.ico" key="favico" />
+        <meta
+          name="description" 
+          content="Teman Gawe is a coffee establishment founded in Yogyakarta, Indonesia. 
+          We provide high quality coffee at competitive prices, because everyone deserves
+          a great coffee."
+        />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,3 @@
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false;
-
-import Head from 'next/head'
-
 import Nav from '../components/nav'
 import Header from '../components/header'
 import About from '../components/about'
@@ -13,28 +7,15 @@ import Footer from '../components/footer'
 export default function IndexPage() {
   return (
     <div>
-      <Head>
-        <title>Teman Gawe Coffee</title>
-        <link rel="icon" href="/public/favicon.ico" key="favico" />
-        <meta
-          name="description" 
-          content="Teman Gawe is a coffee establishment founded in Yogyakarta, Indonesia. 
-          We provide high quality coffee at competitive prices, because everyone deserves
-          a great coffee."
-        />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
-      </Head>
-      <div>
-        <Nav />
-        <div
-          className="relative px-8 sm:px-12 lg:px-16 bg-image"
-        >
-          <Header className="pt-28 pb-8 w-4/5 sm:pt-44 lg:w-2/3 lg:pb-44 xl:w-3/5"/>
-          <About className="sm:mt-16"/>
-        </div>
-        <Menu className="pb-16 px-8 sm:px-12 lg:px-16"/>
-        <Footer className="border-t-2"/>
+      <Nav />
+      <div
+        className="relative px-8 sm:px-12 lg:px-16 bg-image"
+      >
+        <Header className="pt-28 pb-8 w-4/5 sm:pt-44 lg:w-2/3 lg:pb-44 xl:w-3/5"/>
+        <About className="sm:mt-16"/>
       </div>
+      <Menu className="pb-16 px-8 sm:px-12 lg:px-16"/>
+      <Footer className="border-t-2"/>
     </div>
   )
 }
