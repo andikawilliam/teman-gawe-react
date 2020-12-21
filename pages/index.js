7bce06ae704ec5d3ac9efc7838abe52f2f367c0c
vandercoder <andikawilliam@outlook.com>
2020-12-21 22:45:17 +0700

    added custom Head, meta tags and favico

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
+import Head from 'next/head'
+
 import Nav from '../components/nav'
 import Header from '../components/header'
 import About from '../components/about'
@@ -7,15 +13,28 @@ import Footer from '../components/footer'
 export default function IndexPage() {
   return (
     <div>
-      <Nav />
-      <div
-        className="relative px-8 sm:px-12 lg:px-16 bg-coffee-splash bg-no-repeat bg-left-35% bg-1200 h-1500 shadow-bottom-white sm:bg-left-10% lg:h-1800 lg:bg-top lg:bg-cover"
-      >
-        <Header className="pt-28 pb-8 w-4/5 sm:pt-44 lg:w-2/3 lg:pb-44 xl:w-3/5"/>
-        <About className="sm:mt-16"/>
+      <Head>
+        <title>Teman Gawe Coffee</title>
+        <link rel="icon" href="/public/favicon.ico" key="favico" />
+        <meta
+          name="description" 
+          content="Teman Gawe is a coffee establishment founded in Yogyakarta, Indonesia. 
+          We provide high quality coffee at competitive prices, because everyone deserves
+          a great coffee."
+        />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+      </Head>
+      <div>
+        <Nav />
+        <div
+          className="relative px-8 sm:px-12 lg:px-16 bg-image"
+        >
+          <Header className="pt-28 pb-8 w-4/5 sm:pt-44 lg:w-2/3 lg:pb-44 xl:w-3/5"/>
+          <About className="sm:mt-16"/>
+        </div>
+        <Menu className="pb-16 px-8 sm:px-12 lg:px-16"/>
+        <Footer className="border-t-2"/>
       </div>
-      <Menu className="pb-16 px-8 sm:px-12 lg:px-16"/>
-      <Footer className="border-t-2"/>
     </div>
   )
 }
